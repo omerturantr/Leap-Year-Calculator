fada2bbb4724037c2563724e1a68872b8d8ce796
Omer Faruk TURAN <faruktrn25@gmail.com>
2023-10-04 09:50:24 +0300

    LeapYearCalculator
    JAVA Program that checks whether the year received from the user is a LEAP YEAR or Not.

---
+++ LeapYearCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class LeapYearCalculator {
+    public static void main(String[] args) {
+
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Year: ");     year = input.nextInt();
+
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    System.out.println(year + " is a LEAP YEAR.");
+                }
+                else {
+                    System.out.println(year + " is not a LEAP YEAR.");
+                }
+            }
+            else {
+                System.out.println(year + " is a LEAP YEAR.");
+            }
+        }
+        else {
+            System.out.print(year + " is not a LEAP YEAR.");
+        }
+    }
+}
